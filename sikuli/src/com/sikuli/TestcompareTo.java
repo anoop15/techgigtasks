0c8ba9128872e31c729d6f9c0e246139f86bb527
anoop <anu.lovi@gmail.com>
2016-08-22 10:35:54 +0530

    class name changed

---
+++ sikuli/src/com/sikuli/TestcompareTo.java
@@ -0,0 +1,18 @@
+package com.sikuli;
+
+public class TestcompareTo {
+	public static void main(String[] args) {
+		String a="sharm";
+		String b="sharma";
+		int c =a.compareTo(b);
+		System.out.println(c);
+		if(c<0)
+			System.out.println(b+ " comes first");
+		if(c==0)
+			System.out.println(" two strings are lexicographically equivalent");
+		if(c>0)
+			System.out.println(a+ " comes first");
+			
+		
+	}
+}
